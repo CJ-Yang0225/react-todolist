6563503d28db4d34408163b1553e4132b9bf82b0
CJ-Yang <cj.yang0225@gmail.com>
2020-12-27 23:41:16 +0800

    SEO

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.min.css';
-import TaskNavbar from './TaskNavbar';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.min.css";
+import TaskNavbar from "./components/TaskNavbar";
+import TaskAdder from "./components/TaskAdder";
 
 ReactDOM.render(
   <React.StrictMode>
-    <TaskNavbar />
+    <header>
+      <TaskNavbar />
+    </header>
+    <main className="container main">
+      <TaskAdder />
+    </main>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
