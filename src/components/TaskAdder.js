43ac7d93db5639d6680a44b2deeb4aa8bcd9a92c
CJ-Yang <cj.yang0225@gmail.com>
2020-12-27 23:41:02 +0800

    Create TaskAdder.js Component

---
+++ src/components/TaskAdder.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+library.add(fas);
+
+const TaskAdder = (props) => {
+  return (
+    <div className="task__adder">
+      <label htmlFor="adder__input" className="adder__label border-0">
+        <FontAwesomeIcon icon="plus" />
+      </label>
+      <input
+        type="text"
+        className="adder__input"
+        id="adder__input"
+        placeholder="Add Task"
+      />
+      <button className="adder__option border-0">
+        <FontAwesomeIcon icon="ellipsis-v" />
+      </button>
+    </div>
+  );
+};
+
+export default TaskAdder;
