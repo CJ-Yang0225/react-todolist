7ef8e973115d7f2fe5db73048d192847a1ff625c
CJ-Yang0225 <57297482+CJ-Yang0225@users.noreply.github.com>
2020-12-30 00:26:54 +0800

    Update TaskAdder.js Component

--- src/components/TaskAdder.js
+++ src/components/TaskAdder.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import TaskExpansion from "./TaskExpansion";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -6,6 +7,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 library.add(fas);
 
 const TaskAdder = (props) => {
+  
   return (
     <div className="task__adder">
       <label htmlFor="adder__input" className="adder__label border-0">
@@ -20,6 +22,7 @@ const TaskAdder = (props) => {
       <button className="adder__option border-0">
         <FontAwesomeIcon icon="ellipsis-v" />
       </button>
+      <TaskExpansion /* isExpanded={props.taskOption.isExpanded} */ />
     </div>
   );
 };
