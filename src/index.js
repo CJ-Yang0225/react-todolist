1f5ec81bce4372c0eea30b113a5ce054e1ffc009
CJ-Yang0225 <57297482+CJ-Yang0225@users.noreply.github.com>
2020-12-31 23:22:44 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
 import "./styles/index.min.css";
 import TaskNavbar from "./components/TaskNavbar";
 import TaskAdder from "./components/TaskAdder";
+
+library.add(fas, far);
 
 ReactDOM.render(
   <React.StrictMode>
