9407fe99221b9066029fd21f5a57e1a5f37e7e02
CJ-Yang0225 <57297482+CJ-Yang0225@users.noreply.github.com>
2021-01-01 21:42:03 +0800

    Update layout

--- src/components/TaskDetail.js
+++ src/components/TaskDetail.js
@@ -3,43 +3,35 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const TaskDetail = ({ expanded: isExpanded }) => {
   return (
-    <div
-      className="task__expansion"
-      style={{ display: isExpanded ? "" : "none" }}
-    >
-      <div className="expansion__body">
-        <label className="expansion__body__label">
+    <div className="task__detail" style={{ display: isExpanded ? "" : "none" }}>
+      <div className="detail__body">
+        <label className="detail__body__label">
           <FontAwesomeIcon icon={["far", "calendar-alt"]} />
           Deadline
         </label>
-        <div className="expansion__body__deadline">
+        <div className="detail__body__deadline">
           <input type="date" name="date" className="date__input" />
           <input type="time" name="time" className="time__input" />
         </div>
-        <label className="expansion__body__label">
+        <label className="detail__body__label">
           <FontAwesomeIcon icon={["far", "file"]} />
           File
         </label>
-        <label
-          className="expansion__body__label"
-          style={{ display: "inline-block", height: "32px" }}
-        >
-          <div className="expansion__body__file">
-            <FontAwesomeIcon icon={["fas", "plus"]} />
-            <input type="file" name="file" className="file__input" />
-          </div>
+        <label className="detail__body__file" style={{display: "inline-block", height: 32}}>
+          <FontAwesomeIcon icon={["fas", "plus"]} />
+          <input type="file" name="file" className="file__input" />
         </label>
-        <label className="expansion__body__label">
-          <FontAwesomeIcon icon={["far", "message-dots"]} />
+        <label className="detail__body__label">
+          <FontAwesomeIcon icon={["far", "comment-dots"]} />
           Message
         </label>
         <textarea
           name="message"
-          className="expansion__body__description"
+          className="detail__body__description"
           placeholder="Type your task here..."
         ></textarea>
       </div>
-      <div className="expansion__footer">
+      <div className="detail__footer">
         <button className="cancel--danger">
           <FontAwesomeIcon icon={["fas", "times"]} />
           Cancel
