13432798782351783dd377b9ebc93ab0145a2902
CJ-Yang0225 <57297482+CJ-Yang0225@users.noreply.github.com>
2021-01-04 03:20:15 +0800

    Update to controlled component

--- src/components/TaskAdder.js
+++ src/components/TaskAdder.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TaskDetail from "./TaskDetail";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -8,6 +8,12 @@ const TaskAdder = () => {
   const toggleDetail = () =>
     setIsDetailExpanded((isDetailExpanded) => !isDetailExpanded);
 
+  const [taskTitle, setTaskTitle] = useState("");
+
+  const handleTaskTitle = (event) => {
+    setTaskTitle(event.target.value);
+  };
+
   return (
     <div className="task__adder">
       <label htmlFor="adder__input" className="adder__label border-0">
@@ -16,13 +22,16 @@ const TaskAdder = () => {
       <input
         type="text"
         id="adder__input"
+        name="title"
         className="adder__input"
         placeholder="Add Task"
+        value={taskTitle}
+        onChange={handleTaskTitle}
       />
       <button className="adder__option border-0" onClick={toggleDetail}>
         <FontAwesomeIcon icon="ellipsis-v" />
       </button>
-      <TaskDetail expanded={isDetailExpanded} />
+      <TaskDetail title={taskTitle} expanded={isDetailExpanded} />
     </div>
   );
 };
