58ac33da3a2691ec6219db2110fbbe4e7558c1e3
CJ-Yang0225 <cj.yang0225@gmail.com>
2021-08-09 02:08:16 +0800

    feat: add transition animation with react-spring

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { animated, useSpringRef, useTransition, useChain } from "react-spring";
 import TaskNavbar from "./TaskNavbar";
 import TaskCreateForm from "./TaskCreateForm";
 import TaskCard from "./TaskCard";
@@ -49,35 +50,55 @@ const App = () => {
   useEffect(() => saveTasks(tasks), [tasks]);
 
   const setTask = (index) => (task) =>
-    setTasks(([...tasks]) => Object.assign(tasks, { [index]: task }));
+    setTasks((tasks) => Object.assign([...tasks], { [index]: task }));
 
   const addTask = (task) => setTasks((tasks) => [...tasks, task]);
 
   const removeTask = (indexForRemove) => () =>
     setTasks((tasks) => tasks.filter((_, index) => index !== indexForRemove));
 
+  const filteredTasks = tasks
+    .map((task, index) => [task, index])
+    .filter(([task]) => TaskFilter[taskFilterType](task))
+    .sort(([taskA], [taskB]) => taskB.isFavorite - taskA.isFavorite)
+    .map(([task, index]) => (
+      <TaskCard
+        key={index}
+        value={task}
+        onUpdate={setTask(index)}
+        onRemove={removeTask(index)}
+      />
+    ));
+
+  const tasksRef = useSpringRef();
+
+  const transitionTasks = useTransition(filteredTasks, {
+    ref: tasksRef,
+    keys: new Array(filteredTasks.length).fill().map((_, index) => index),
+    trail: 500 / filteredTasks.length,
+    from: { opacity: 0, scale: 0 },
+    enter: { opacity: 1, scale: 1 },
+    leave: { opacity: 0, scale: 0 },
+  });
+
+  useChain([tasksRef], [0.5]);
+
   return (
-    <>
+    <React.Fragment>
       <header>
         <TaskNavbar value={taskFilterType} onChange={setTaskFilterType} />
       </header>
       <main className="container main">
         <TaskCreateForm onCreate={addTask} />
-
-        {tasks
-          .map((task, index) => [task, index])
-          .filter(([task]) => TaskFilter[taskFilterType](task))
-          .sort(([taskA], [taskB]) => taskB.isFavorite - taskA.isFavorite)
-          .map(([task, index]) => (
-            <TaskCard
-              key={index}
-              value={task}
-              onUpdate={setTask(index)}
-              onRemove={removeTask(index)}
-            />
-          ))}
+        {transitionTasks((style, task) => (
+          <animated.div
+            className="task-card__wrapper"
+            style={style}
+            children={task}
+          />
+        ))}
       </main>
-    </>
+    </React.Fragment>
   );
 };
 
